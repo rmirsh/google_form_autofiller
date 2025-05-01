be38c933e4c7b3c2de1d78963c277f9c7771d49b
k.ayupov <ayupov.k@romir.ru>
2025-05-01 16:29:43 +0300

    Add interfaces

--- internal/application/formservice/interfaces.go
+++ internal/application/formservice/interfaces.go
@@ -1,1 +1,19 @@
 package formservice
+
+import "github.com/rmirsh/google_form_autofiller/internal/domain/form"
+
+type Fetcher interface {
+	Fetch(url string) (string, error)
+}
+
+type Parser interface {
+	Parse(html string) (form.Form, error)
+}
+
+type Filler interface {
+	Fill(field Form.Field) any
+}
+
+type Submitter interface {
+	Submit(url string, payload map[string]string) error
+}
